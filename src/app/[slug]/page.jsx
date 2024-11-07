5ce858bec29edad504bbdcb1616aae70dd6c27c9
Eric Schmidt <e.ryan.schmidt@gmail.com>
2024-11-07 09:39:55 -0700

    Update Experiences to be fetched server-side.

--- src/app/[slug]/page.jsx
+++ src/app/[slug]/page.jsx
@@ -1,14 +1,42 @@
 import React from "react";
 import { draftMode } from "next/headers";
+import {
+  detachExperienceStyles,
+  fetchBySlug,
+} from "@contentful/experiences-sdk-react";
+import { getClient } from "@/src/lib/client";
 import { Experience } from "@/src/components/Experience";
 
-const Page = ({ params }) => {
+const Page = async ({ params }) => {
   // Check if Draft Mode is enabled.
   const { isEnabled } = draftMode();
   // TODO: Can't set the cookie on localhost with Live Preview, so preview can be forced to `true` here.
   // const isEnabled = true;
 
-  return <Experience slug={params.slug} preview={isEnabled} />;
+  const localeCode = "en-US";
+
+  const experience = await fetchBySlug({
+    client: await getClient({ preview: isEnabled }),
+    slug: params.slug,
+    experienceTypeId: "page",
+    localeCode,
+  });
+
+  // Extract the styles from the experience for manual placement.
+  // If these are not placed, there may be some odd flashes of unstyled content.
+  const stylesheet = experience ? detachExperienceStyles(experience) : null;
+
+  // The experience is not serializable by Next.js directly due to their strict JSON serialization techniques,
+  // so we need to serialize the experience ourselves and pass the JSON string as a prop to the component.
+  // @see https://www.contentful.com/developers/docs/experiences/using-with-nextjs/#server-side-rendering-using-app-router
+  const experienceJSON = experience ? JSON.stringify(experience) : null;
+
+  return (
+    <>
+      {stylesheet && <style>{stylesheet}</style>}
+      <Experience experience={experienceJSON} localeCode={localeCode} />
+    </>
+  );
 };
 
 export default Page;
--- src/components/Experience.jsx
+++ src/components/Experience.jsx
@@ -8,31 +8,7 @@ import {
   defineDesignTokens,
   useFetchBySlug,
 } from "@contentful/experiences-sdk-react";
-import { getClient } from "@/src/lib/client";
 
-// These could also be dynamic.
-const experienceTypeId = "page";
-const localeCode = "en-US";
-
-export const Experience = ({ slug, preview }) => {
-  // Fetch the experience from Contentful by the slug.
-  const { experience, isLoading, error } = useFetchBySlug({
-    client: getClient({ preview }),
-    experienceTypeId,
-    slug,
-    localeCode,
-  });
-
-  // console.log(isLoading);
-  // console.log(error);
-
-  // if (isLoading) return <h1>Loading...</h1>;
-  // if (error)
-  //   return (
-  //     <div className="bg-red-100 w-full py-24 rounded-lg text-center">
-  //       Error: {error.message}
-  //     </div>
-  //   );
-
+export const Experience = ({ experience, localeCode }) => {
   return <ExperienceRoot experience={experience} locale={localeCode} />;
 };
